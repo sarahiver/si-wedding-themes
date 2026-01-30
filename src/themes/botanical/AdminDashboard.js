fcf3702ec04c6c2a75907b9d0b117a49b4520d87
ivergentz <67914070+ivergentz@users.noreply.github.com>
2026-01-30 17:48:16 +0100

    Add files via upload

--- src/themes/botanical/AdminDashboard.js
+++ src/themes/botanical/AdminDashboard.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import { AdminShell } from '../../components/admin/core';
-import { BotanicalAdminStyles } from '../../components/admin/styles/BotanicalAdminStyles';
+
+const BotanicalAdminWrapper = styled.div`
+  --forest: #2D3B2D;
+  --sage: #8B9D83;
+  --sage-dark: #6B7D63;
+  --sage-light: rgba(139, 157, 131, 0.3);
+  --sage-muted: rgba(139, 157, 131, 0.15);
+  --cream: #F9F6F2;
+  --cream-dark: #EDE9E3;
+  --text: #2D3B2D;
+  --text-light: #5A6B5A;
+  --text-muted: #8A9B8A;
+  
+  min-height: 100vh;
+  font-family: 'Lato', -apple-system, BlinkMacSystemFont, sans-serif;
+`;
 
 function AdminDashboard() {
   return (
-    <BotanicalAdminStyles>
+    <BotanicalAdminWrapper>
       <AdminShell themeName="botanical" />
-    </BotanicalAdminStyles>
+    </BotanicalAdminWrapper>
   );
 }
 
