6080617c125391e6e2843369bc3a8740211202ef
ivergentz <67914070+ivergentz@users.noreply.github.com>
2026-02-05 10:26:39 +0100

    Delete src/neon directory

--- src/neon/AdminDashboard.js
+++
@@ -1,4 +0,0 @@
-// Neon Theme - Uses Editorial Admin Style
-// TODO: Create dedicated NeonAdminStyles when admin theming is refactored
-import { EditorialAdmin } from "../../components/admin";
-export default EditorialAdmin;
