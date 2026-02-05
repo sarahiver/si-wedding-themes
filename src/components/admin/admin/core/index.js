02c06d66d06f5276ce80963b810efa12fdabbd80
ivergentz <67914070+ivergentz@users.noreply.github.com>
2026-02-05 09:20:20 +0100

    Delete src/components/admin/admin directory

--- src/components/admin/admin/core/index.js
+++
@@ -1,4 +0,0 @@
-export { AdminProvider, useAdmin } from './AdminContext';
-export { default as AdminShell } from './AdminShell';
-export * from './sections';
-export * from './editors';
--- src/components/admin/admin/core/sections/index.js
+++
@@ -1,6 +0,0 @@
-export { default as DashboardSection } from './DashboardSection';
-export { default as RSVPSection } from './RSVPSection';
-export { default as GuestbookSection } from './GuestbookSection';
-export { default as MusicSection } from './MusicSection';
-export { default as PhotosSection } from './PhotosSection';
-export { default as StatusSection } from './StatusSection';
