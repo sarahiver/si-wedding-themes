33b228d2379023b4a038b37cf96223d0a922d9ea
ivergentz <ivergentz@gmnail.com>
2026-02-11 20:29:16 +0100

    Fix neon admin: use NeonAdmin instead of EditorialAdmin
    Neon theme admin login + dashboard were showing Editorial styling.
    NeonAdminStyles.js already existed with proper neon pink/cyan styling.

--- src/themes/neon/AdminDashboard.js
+++ src/themes/neon/AdminDashboard.js
@@ -1,4 +1,2 @@
-// Neon Theme - Uses Editorial Admin Style
-// TODO: Create dedicated NeonAdminStyles when admin theming is refactored
-import { EditorialAdmin } from "../../components/admin";
-export default EditorialAdmin;
+import { NeonAdmin } from "../../components/admin";
+export default NeonAdmin;
